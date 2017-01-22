680d0acb13758279b06296152f59cdcc80a561d7
Romer Ramos <romerramos@devtohack.com>
2017-01-22 13:38:32 -0400

    Fixes en creditos

--- src/states/Credits.js
+++ src/states/Credits.js
@@ -7,35 +7,55 @@ export default class extends Phaser.State {
   }
 
   create () {
-    let endScreen;
+    // let endScreen;
+    //
+    // endScreen = this.game.add.sprite(0, 0, 'end_screen');
+    // endScreen.width = this.world.width;
+    // endScreen.height = this.world.height;
 
-    endScreen = this.game.add.sprite(0, 0, 'end_screen');
-    endScreen.width = this.world.width;
-    endScreen.height = this.world.height;
     const bannerText = 'Credits'
     let banner = this.add.text(this.world.centerX, 50, bannerText)
     banner.font = 'Bangers'
     banner.padding.set(10, 16)
     banner.fontSize = 40
-    banner.fill = '#77BFA3'
+    banner.fill = '#E37F26'
     banner.smoothed = false
     banner.anchor.setTo(0.5)
     let menu = this.add.text(this.world.width - 100, 50, 'Back')
-    
+
     menu.anchor.setTo(0.5)
     menu.inputEnabled = true
     menu.events.onInputUp.add(this.menuOnClick, this)
 
     let cbanner = this.add.text(this.world.centerX, 90, 'Conceptualizacion')
+    cbanner.fill = '#E37F26'
+
     let ctext = this.add.text(this.world.centerX, 120, 'Todos')
+    ctext.fill = '#E37F26'
+
     let dbanner = this.add.text(this.world.centerX, 150, 'Development')
+    dbanner.fill = '#E37F26'
+
     let dtext = this.add.text(this.world.centerX, 230,' Romer Ramos / Jorge Basilio\nNelson Marcano / Jose de Leon\n     Victor Padula')
+    dtext.fill = '#E37F26'
+
     let dsbanner = this.add.text(this.world.centerX, 315,'Ilustration')
+    dsbanner.fill = '#E37F26'
+
     let dstext = this.add.text(this.world.centerX, 350,' Dagriz Noguera / Densel Gutierrez')
+    dstext.fill = '#E37F26'
+
     let mbanner = this.add.text(this.world.centerX, 395, 'Music')
+    mbanner.fill = '#E37F26'
+
     let mtext = this.add.text(this.world.centerX, 435,'Juan Manrique\nDensel Gutierrez')
-    let abanner = this.add.text(this.world.centerX, 475, 'Special Thanks')
-    let atext = this.add.text(this.world.centerX, 500,'Anais Legonia  Maria Romero  Jorge Fuentes  Academia DevToHack')
+    mtext.fill = '#E37F26'
+
+    let abanner = this.add.text(this.world.centerX, 490, 'Special Thanks')
+    abanner.fill = '#E37F26'
+
+    let atext = this.add.text(this.world.centerX, 520,'Anais Legonia  Maria Romero  Jorge Fuentes  Academia DevToHack')
+    atext.fill = '#E37F26'
 
     let textarray = [ctext,dtext,dstext,mtext,atext]
     for(let text of textarray){
@@ -48,7 +68,7 @@ export default class extends Phaser.State {
       bnr.anchor.setTo(0.5)
       bnr.padding.set(10, 16)
       //bnr.fill = '#77BFA3'
-      bnr.smoothed = false 
+      bnr.smoothed = false
     }
   }
   menuOnClick(){
